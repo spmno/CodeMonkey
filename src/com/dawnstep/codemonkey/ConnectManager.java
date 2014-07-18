dcbc3e532325d805fafa6f69e20c0fb9bf576d44
spmno <sunqingpeng@hotmail.com>
2014-07-18 09:07:13 +0800

    add implement for ConnectManager

--- src/com/dawnstep/codemonkey/ConnectManager.java
+++ src/com/dawnstep/codemonkey/ConnectManager.java
@@ -1,5 +1,35 @@
 package com.dawnstep.codemonkey;
 
-public class ConnectManager {
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 
+public class ConnectManager {
+	
+	enum ConnectStatus {
+		PHONE_CONNECTED,
+		WIFI_CONNECTED,
+		PHONE_AND_WIFI_CONNECTED,
+		NONE_CONNECTED,
+	}
+	
+	ConnectStatus getNetStatus(Context context) {
+		ConnectivityManager connectivityManager = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo activeInfo = connectivityManager.getActiveNetworkInfo();
+		if (activeInfo != null && activeInfo.isConnected()) {
+		     boolean wifiConnected = activeInfo.getType() == ConnectivityManager.TYPE_WIFI;
+	         boolean mobileConnected = activeInfo.getType() == ConnectivityManager.TYPE_MOBILE;
+	         if (wifiConnected && mobileConnected) {
+	        	 return ConnectStatus.PHONE_AND_WIFI_CONNECTED;
+	         } else if (wifiConnected) {
+	        	 return ConnectStatus.WIFI_CONNECTED;
+	         } else {
+	        	 return ConnectStatus.PHONE_CONNECTED;
+	         }
+
+		} else {
+			return ConnectStatus.NONE_CONNECTED;
+		}
+	}
+	
 }
